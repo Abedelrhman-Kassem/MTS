3e9ec9b9a420c36a186c1136d666baa848d6e7cb
Abedelrhman-Kassem <107558049+Abedelrhman-Kassem@users.noreply.github.com>
2023-10-11 15:22:54 +0300

    remove animations add goverenment

--- js/main.js
+++ js/main.js
@@ -2,32 +2,6 @@ let links = document.getElementsByClassName("nav-link");
 let sections = document.querySelectorAll("section");
 let navbar = document.querySelector(".navbar");
 
-// add CLASSES TO THE SECTIONS
-function addHideClass(sections) {
-  for (let i = 0; i < sections.length; i++) {
-    i % 2 == 0
-      ? sections[i].classList.add("hide-right")
-      : sections[i].classList.remove("hide-left");
-  }
-}
-
-// put animations on sections
-function secAnimate(section) {
-  if (section.getBoundingClientRect().top < 600) {
-    section.classList.add("show");
-    section.classList.remove("hide");
-  } else {
-    section.classList.add("hide");
-    section.classList.remove("show");
-  }
-}
-
-window.addEventListener("scroll", () => {
-  for (let i = 0; i < sections.length; i++) {
-    secAnimate(sections[i]);
-  }
-});
-
 // SCROLL TO THE SECTION WHEN LINK CLICKED
 Array.from(links).forEach((link) => {
   link.addEventListener("click", function (e) {
